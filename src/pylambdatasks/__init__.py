d675907ba68ffd1282f29fd902b6553fbd8442c9
oktylab <oktylab@gmail.com>
2026-01-29 00:49:23 +0100

    fix: increase the version to 0.1.16

--- src/pylambdatasks/__init__.py
+++ src/pylambdatasks/__init__.py
@@ -3,6 +3,6 @@ from .dependencies import DependsFactory as Depends
 from .dependencies import LambdaEvent, LambdaContext
 from .task import Task
 
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 
 __all__ = ["LambdaTasks", "Depends", "LambdaEvent", "LambdaContext", "Task"]
